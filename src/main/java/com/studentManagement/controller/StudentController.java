2e4535acdc4d99a25f6855284867c0e2c7001af7
Pablo Martin Lopez <pablo_mtv08@hotmail.com>
2021-08-12 23:39:37 +0200

    Se terminaron de agregar los controlladores de Student.

--- src/main/java/com/studentManagement/controller/StudentController.java
+++ src/main/java/com/studentManagement/controller/StudentController.java
@@ -3,10 +3,9 @@ package com.studentManagement.controller;
 import com.studentManagement.model.Student;
 import com.studentManagement.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -19,5 +18,31 @@ public class StudentController {
     public Student createStudent(@RequestBody Student student){
         return studentService.createStudent(student);
     }
+
+    @PutMapping("update-student")
+    public Student updateStudent(@RequestBody Student student){
+        return studentService.createStudent(student);
+    }
+
+    
+    @GetMapping("get-sudent-by-dni")
+    public Student getStudentByDni(@RequestParam("dni") String dni){
+        return studentService.getStudentByDni(dni);
+    }
+    
+    
+    @GetMapping("list-students")
+    public List<Student> listStudents(){
+        return studentService.getListStudents();
+    }
+
+
+    @DeleteMapping("delete-student-by-dni")
+    public void deleteStudent(@RequestParam("dni") String dni){
+        studentService.deleteStudentByDni(dni);
+    }
+
+
+
 
 }
